1369cdcd15eb9e2d486aec70a957f77b0a36d150
factwixard <factwixard@gmail.com>
2025-06-24 21:58:01 -0700

    loc

---
+++ localization/en-us.lua
@@ -0,0 +1,232 @@
+return {
+	descriptions = {
+		Tag = {
+			tag_upgrd_deckbuild = {
+				name = 'Deck-Build Tag',
+				text = {
+					'Gives a free',
+					'{C:attention}Mega Upgrade Pack{}',
+				},
+			},
+		},
+		Other = {
+			undiscovered_upgrd_upgrade = {
+				name = 'Not Discovered',
+				text = {
+					'Purchase or use',
+					'this card in an',
+					'unseeded run to',
+					'learn what it does',
+				},
+			},
+			p_upgrd_upgrdpack1 = {
+				name = 'Upgrade Pack',
+				text = {
+					'Choose {C:attention}#1#{} of up to',
+					'{C:attention}#2#{C:upgrd_upgrade} Upgrade{} cards',
+				},
+			},
+			p_upgrd_upgrdpack2 = {
+				name = 'Upgrade Pack',
+				text = {
+					'Choose {C:attention}#1#{} of up to',
+					'{C:attention}#2#{C:upgrd_upgrade} Upgrade{} cards',
+				},
+			},
+			p_upgrd_upgrdpack3 = {
+				name = 'Upgrade Pack',
+				text = {
+					'Choose {C:attention}#1#{} of up to',
+					'{C:attention}#2#{C:upgrd_upgrade} Upgrade{} cards',
+				},
+			},
+			p_upgrd_upgrdpack4 = {
+				name = 'Upgrade Pack',
+				text = {
+					'Choose {C:attention}#1#{} of up to',
+					'{C:attention}#2#{C:upgrd_upgrade} Upgrade{} cards',
+				},
+			},
+			p_upgrd_upgrdpack1_j = {
+				name = 'Jumbo Upgrade Pack',
+				text = {
+					'Choose {C:attention}#1#{} of up to',
+					'{C:attention}#2#{C:upgrd_upgrade} Upgrade{} cards',
+				},
+			},
+			p_upgrd_upgrdpack2_j = {
+				name = 'Jumbo Upgrade Pack',
+				text = {
+					'Choose {C:attention}#1#{} of up to',
+					'{C:attention}#2#{C:upgrd_upgrade} Upgrade{} cards',
+				},
+			},
+			p_upgrd_upgrdpack1_m = {
+				name = 'Mega Upgrade Pack',
+				text = {
+					'Choose {C:attention}#1#{} of up to',
+					'{C:attention}#2#{C:upgrd_upgrade} Upgrade{} cards',
+				},
+			},
+			p_upgrd_upgrdpack2_m = {
+				name = 'Mega Upgrade Pack',
+				text = {
+					'Choose {C:attention}#1#{} of up to',
+					'{C:attention}#2#{C:upgrd_upgrade} Upgrade{} cards',
+				},
+			},
+		},
+		Spectral = {
+			c_upgrd_finalize = {
+				name = 'Finalize',
+				text = {
+					{
+						'Adds {C:attention}every listed bonus{}',
+						'to {C:attention}#1#{} selected playing cards',
+					},
+					{
+						'{C:chips}+#2#{} Chips,',
+						'{C:mult}+#3#{} Mult,',
+						'{X:chips,C:white}X#4#{} Chips,',
+						'{X:mult,C:white}X#5#{} Mult,',
+						'{C:gold}+#6#{} Dollars',
+					},
+				},
+			},
+		},
+		upgrd_upgrade = {
+			c_upgrd_chippi = {
+				name = 'Chippi',
+				text = {
+					'Adds {C:chips}+#2#{} Chips permanently',
+					'to {C:attention}#1#{} selected playing cards',
+				},
+			},
+			c_upgrd_multy = {
+				name = 'Multy',
+				text = {
+					'Adds {C:mult}+#2#{} Mult permanently',
+					'to {C:attention}#1#{} selected playing cards',
+				},
+			},
+			c_upgrd_chipliply = {
+				name = 'Chipliply',
+				text = {
+					'Adds {X:chips,C:white}X#2#{} Chips permanently',
+					'to {C:attention}#1#{} selected playing cards',
+				},
+			},
+			c_upgrd_multiplicate = {
+				name = 'Multiplicate',
+				text = {
+					'Adds {X:mult,C:white}X#2#{} Mult permanently',
+					'to {C:attention}#1#{} playing cards',
+				},
+			},
+			c_upgrd_dollarify = {
+				name = 'Dollarify',
+				text = {
+					'Adds {C:gold}+#2#{} Dollars permanently',
+					'to {C:attention}#1#{} playing cards',
+				},
+			},
+			c_upgrd_handimate = {
+				name = 'Handimate',
+				text = {
+					{
+						'Adds {C:attention}every listed bonus{}',
+						'as {C:attention}held in hand bonuses{}',
+						'to {C:attention}#1#{} playing cards',
+					},
+					{
+						'{C:chips}+#2#{} Chips,',
+						'{C:mult}+#3#{} Mult,',
+						'{X:chips,C:white}X#4#{} Chips,',
+						'{X:mult,C:white}X#5#{} Mult,',
+						'{C:gold}+#6#{} Dollars'
+					},
+				},
+			},
+			c_upgrd_planetrical = {
+				name = 'Planetrical',
+				text = {
+					'{C:attention}Doubles{} {C:chips}Chips{} and {C:mult}Mult{}',
+					'given by {C:attention}level ups{}',
+				},
+			},
+			c_upgrd_levelevel = {
+				name = 'LeveleveL',
+				text = {
+					'{C:attention}Quadruples{} {C:chips}Chips{} and {C:mult}Mult{}',
+					"of most played hand's {C:attentin}level ups{}",
+				},
+			},
+			c_upgrd_swordity = {
+				name = 'Swordity',
+				text = {
+					'Gives all {C:spades}Spades{} in {C:attention}full deck{}',
+					'{C:chips}+#1#{} Chips',
+				},
+			},
+			c_upgrd_coinraliry = {
+				name = 'Coinraliry',
+				text = {
+					'Gives all {C:diamonds}Diamonds{} in {C:attention}full deck{}',
+					'{C:chips}+#1#{} Chips',
+				},
+			},
+			c_upgrd_wandolotry = {
+				name = 'Wandolotry',
+				text = {
+					'Gives all {C:clubs}Clubs{} in {C:attention}full deck{}',
+					'{C:chips}+#1#{} Chips',
+				},
+			},
+			c_upgrd_cupriscity = {
+				name = 'Cupriscity',
+				text = {
+					'Gives all {C:hearts}Hearts{} in {C:attention}full deck{}',
+					'{C:chips}+#1#{} Chips',
+				},
+			},
+			c_upgrd_arrowfall = {
+				name = 'Arrowfall',
+				text = {
+					'Gives all {C:spades}Spades{} in {C:attention}hand{}',
+					'{C:chips}+#1#{} Chips',
+				},
+			},
+			c_upgrd_cutgem = {
+				name = 'Cut Gem',
+				text = {
+					'Gives all {C:diamonds}Diamonds{} in {C:attention}hand{}',
+					'{C:gold}+#1#{} Dollars',
+				},
+			},
+			c_upgrd_onyxorb = {
+				name = 'Onyx Orb',
+				text = {
+					'Gives all {C:clubs}Clubs{} in {C:attention}hand{}',
+					'{C:mult}+#1#{} Mult',
+				},
+			},
+			c_upgrd_bloodrush = {
+				name = 'Bloodrush',
+				text = {
+					'Gives all {C:hearts}Hearts{} in {C:attention}hand{}',
+					'{X:mult,C:white}X#1#{} Mult',
+				},
+			},
+		},
+	},
+	misc = {
+		dictionary = {
+			b_upgrd_upgrade_cards = 'Upgrade Cards',
+			upgrd_upgrade_pack = 'Upgrade Pack',
+			k_upgrd_upgrade = 'Upgrade',
+		},
+		labels = {
+			upgrd_upgrade = 'Upgrade',
+		},
+	},
+}
